dcca6eca2b35aa19cfed30f60699e10f41254842
Viktor Petrushenko <vvlliicc@gmail.com>
2020-04-09 17:47:52 +0300

    fix travis

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,20 +11,6 @@ provider "google" {
   project = var.project
   region  = var.region
 }
-resource "google_compute_firewall" "firewall_puma" {
-  name = "allow-puma-default"
-  # Название сети, в которой действует правило
-  network = "default"
-  # Какой доступ разрешить
-  allow {
-    protocol = "tcp"
-    ports    = ["9292"]
-  }
-  # Каким адресам разрешаем доступ
-  source_ranges = ["0.0.0.0/0"]
-  # Правило применимо для инстансов с перечисленными тэгами
-  target_tags = ["reddit-app"]
-}
 
 resource "google_compute_instance" "app" {
   name         = "reddit-app"
@@ -66,5 +52,19 @@ resource "google_compute_instance" "app" {
   }
 
 
+}
+ resource "google_compute_firewall" "firewall_puma" {
+  name = "allow-puma-default"
+  # Название сети, в которой действует правило
+  network = "default"
+  # Какой доступ разрешить
+  allow {
+    protocol = "tcp"
+    ports    = ["9292"]
+  }
+  # Каким адресам разрешаем доступ
+  source_ranges = ["0.0.0.0/0"]
+  # Правило применимо для инстансов с перечисленными тэгами
+  target_tags = ["reddit-app"]
 }
 
