6b32ae9b252ec8e5d7c7b5d594235c8ff42f0d00
Viktor Petrushenko <vvlliicc@gmail.com>
2020-04-09 15:39:13 +0300

    homework terraform

---
+++ terraform/main.tf
@@ -0,0 +1,70 @@
+terraform {
+  # Версия terraform
+  required_version = "0.12.24"
+}
+
+provider "google" {
+  credentials = file("/home/viktor/Downloads/Infra-39d53002ed4b.json")
+  # Версия провайдера
+  version = "2.15"
+  # ID проекта
+  project = var.project
+  region  = var.region
+}
+resource "google_compute_firewall" "firewall_puma" {
+  name = "allow-puma-default"
+  # Название сети, в которой действует правило
+  network = "default"
+  # Какой доступ разрешить
+  allow {
+    protocol = "tcp"
+    ports    = ["9292"]
+  }
+  # Каким адресам разрешаем доступ
+  source_ranges = ["0.0.0.0/0"]
+  # Правило применимо для инстансов с перечисленными тэгами
+  target_tags = ["reddit-app"]
+}
+
+resource "google_compute_instance" "app" {
+  name         = "reddit-app"
+  machine_type = "g1-small"
+  zone         = var.region
+  tags         = ["reddit-app"]
+
+  boot_disk {
+    initialize_params {
+      image = var.disk_image
+    }
+  }
+  metadata = {
+    # путь до публичного ключа
+    ssh-keys = "appuser:${file(var.public_key_path)}"
+  }
+
+  network_interface {
+    network = "default"
+    access_config {}
+  }
+
+  connection {
+    type  = "ssh"
+    host  = self.network_interface[0].access_config[0].nat_ip
+    user  = "appuser"
+    agent = false
+    # путь до приватного ключа
+    private_key = file(var.private_key_path)
+  }
+
+
+  provisioner "file" {
+    source      = "files/puma.service"
+    destination = "/tmp/puma.service"
+  }
+  provisioner "remote-exec" {
+    script = "files/deploy.sh"
+  }
+
+
+}
+
