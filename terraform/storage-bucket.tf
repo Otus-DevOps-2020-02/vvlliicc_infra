c30022feff1784584aba4c9e55029ad4d5770831
Viktor Petrushenko <vvlliicc@gmail.com>
2020-05-03 14:57:13 +0300

    fixed hm tf-2

--- terraform/storage-bucket.tf
+++ terraform/storage-bucket.tf
@@ -1,5 +1,5 @@
 provider "google" {
-  version = "~> 2.15"
+  version = "~> 0.12.19"
   project = var.project
   region  = var.region
 }
@@ -9,7 +9,7 @@ module "storage-bucket" {
   version = "0.3.0"
 
   # Имя поменяйте на другое
-  name = "storage-bucket-test"
+  name = "storage-test"
 }
 
 output storage-bucket_url {
