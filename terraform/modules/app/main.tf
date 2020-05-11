6a78f25407f30989f34d6c5a6807104bd7b983ed
vvlliicc <vvlliicc@gmail.com>
2020-05-11 23:44:15 +0300

    modify infra

--- terraform/modules/app/main.tf
+++ terraform/modules/app/main.tf
@@ -6,12 +6,14 @@ resource "google_compute_instance" "app" {
   boot_disk {
     initialize_params { image = var.app_disk_image }
   }
+
   network_interface {
     network = "default"
     access_config {
       nat_ip = google_compute_address.app_ip.address
     }
   }
+
   metadata={
     ssh-keys = "appuser:${file(var.public_key_path)}"
   }
--- terraform/modules/db/main.tf
+++ terraform/modules/db/main.tf
@@ -10,13 +10,14 @@ resource "google_compute_instance" "db" {
   }
   network_interface {
     network = "default"
-#    access_config = {}
+    access_config {}
   }
-
-  metadata={
+  metadata = {
     ssh-keys = "appuser:${file(var.public_key_path)}"
   }
- }
+#  tags = ["db"]
+}
+
 resource "google_compute_firewall" "firewall_mongo" {
   name = "allow-mongo-default"
   network = "default"
