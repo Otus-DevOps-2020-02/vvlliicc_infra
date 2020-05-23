103225da55245ac2a8ad92cf7eeff3e1d184b29e
vvlliicc <vvlliicc@gmail.com>
2020-05-23 23:26:13 +0300

    fixed

--- terraform/modules/app/main.tf
+++ terraform/modules/app/main.tf
@@ -27,7 +27,7 @@ resource "google_compute_firewall" "firewall_puma" {
   network = "default"
   allow {
   protocol = "tcp"
-  ports    = ["9292","80"]
+  ports    = ["9292"]
   }
 source_ranges = ["0.0.0.0/0"]
  target_tags = ["reddit-app"]
